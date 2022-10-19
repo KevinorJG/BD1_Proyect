f7014aee7384a44c2844aeff099a7f14a0d4968f
Kevin Ortiz <kevinjair2003@gmail.com>
2022-10-19 00:37:18 -0600

    Archivos del proyecto

---
+++ Financiera.Presentation/Forms/Login/LoginForm.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Financiera.Presentation.Forms.Login
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panelActions = new System.Windows.Forms.Panel();
+            this.btClose = new System.Windows.Forms.Button();
+            this.timerLogin = new System.Windows.Forms.Timer(this.components);
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.BorderlessFormLogin = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.btLogin = new RJCodeAdvance.RJControls.RJButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panelActions.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelActions
+            // 
+            this.panelActions.BackColor = System.Drawing.Color.White;
+            this.panelActions.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panelActions.Controls.Add(this.btClose);
+            this.panelActions.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelActions.Location = new System.Drawing.Point(0, 0);
+            this.panelActions.Name = "panelActions";
+            this.panelActions.Size = new System.Drawing.Size(647, 37);
+            this.panelActions.TabIndex = 0;
+            this.panelActions.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelActions_MouseDown);
+            // 
+            // btClose
+            // 
+            this.btClose.BackColor = System.Drawing.Color.White;
+            this.btClose.BackgroundImage = global::Financiera.Presentation.Properties.Resources.remove;
+            this.btClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btClose.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btClose.FlatAppearance.BorderSize = 0;
+            this.btClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btClose.ForeColor = System.Drawing.Color.White;
+            this.btClose.Location = new System.Drawing.Point(594, 0);
+            this.btClose.Name = "btClose";
+            this.btClose.Size = new System.Drawing.Size(53, 37);
+            this.btClose.TabIndex = 0;
+            this.btClose.UseVisualStyleBackColor = false;
+            this.btClose.Click += new System.EventHandler(this.btClose_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::Financiera.Presentation.Properties.Resources.profile;
+            this.pictureBox1.Location = new System.Drawing.Point(311, 66);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = global::Financiera.Presentation.Properties.Resources.password;
+            this.pictureBox2.Location = new System.Drawing.Point(311, 162);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(398, 89);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.PlaceholderText = "Nombre de usuario";
+            this.txtUser.Size = new System.Drawing.Size(174, 23);
+            this.txtUser.TabIndex = 5;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(398, 184);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PlaceholderText = "Contraseña";
+            this.txtPassword.Size = new System.Drawing.Size(174, 23);
+            this.txtPassword.TabIndex = 6;
+            // 
+            // BorderlessFormLogin
+            // 
+            this.BorderlessFormLogin.AnimationType = Guna.UI2.WinForms.Guna2BorderlessForm.AnimateWindowType.AW_VER_NEGATIVE;
+            this.BorderlessFormLogin.BorderRadius = 5;
+            this.BorderlessFormLogin.ContainerControl = this;
+            this.BorderlessFormLogin.DockIndicatorTransparencyValue = 0.6D;
+            this.BorderlessFormLogin.ResizeForm = false;
+            this.BorderlessFormLogin.TransparentWhileDrag = true;
+            // 
+            // btLogin
+            // 
+            this.btLogin.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.btLogin.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.btLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btLogin.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.btLogin.BorderRadius = 2;
+            this.btLogin.BorderSize = 0;
+            this.btLogin.FlatAppearance.BorderSize = 0;
+            this.btLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btLogin.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btLogin.ForeColor = System.Drawing.Color.White;
+            this.btLogin.Location = new System.Drawing.Point(413, 270);
+            this.btLogin.Name = "btLogin";
+            this.btLogin.Size = new System.Drawing.Size(150, 40);
+            this.btLogin.TabIndex = 7;
+            this.btLogin.Text = "Iniciar Sesión";
+            this.btLogin.TextColor = System.Drawing.Color.White;
+            this.btLogin.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 323);
+            this.panel1.TabIndex = 8;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(647, 360);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelActions);
+            this.ForeColor = System.Drawing.Color.Transparent;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LoginForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.LoginForm_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.LoginForm_Paint_1);
+            this.panelActions.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelActions;
+        private System.Windows.Forms.Button btClose;
+        private System.Windows.Forms.Timer timerLogin;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox txtPassword;
+        private Guna.UI2.WinForms.Guna2BorderlessForm BorderlessFormLogin;
+        private RJCodeAdvance.RJControls.RJButton btLogin;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Financiera.Presentation/Forms/Login/LoginForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace Financiera.Presentation.Forms.Login
+{
+    public partial class LoginForm : Form
+    {
+       
+        public LoginForm()
+        {
+            InitializeComponent();
+            
+        }
+        #region Methods
+
+        void IncreaseOpacity(object sender, EventArgs e)
+        {
+            if (this.Opacity <= 1)  //replace 0.88 with whatever you want
+            {
+                this.Opacity += 0.01;  //replace 0.01 with whatever you want
+            }
+            if (this.Opacity == 1) //replace 0.88 with whatever you want
+                timerLogin.Stop();
+        }
+        #endregion
+
+        private void btClose_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+        private void panelActions_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void LoginForm_Load(object sender, EventArgs e)
+        {
+            this.Opacity = .01;
+            timerLogin.Interval = 2; //replace 10 with whatever you want
+            timerLogin.Tick += IncreaseOpacity;
+            timerLogin.Start();//timer1.Start();
+        }
+
+        private void LoginForm_Paint_1(object sender, PaintEventArgs e)
+        {
+
+            Graphics g = e.Graphics;
+            //Pen pen = new Pen(Color.FromArgb(255, 101, 179));
+
+            
+            Rectangle area = new Rectangle(0, 0, this.Width, this.Height);
+            LinearGradientBrush lgb = new LinearGradientBrush
+           (area, (Color.FromArgb(255, 101, 179), Color.FromArgb(255, 89, 192), Color.FromArgb(255, 78, 205), Color.FromArgb(255, 69, 217), Color.FromArgb(255, 63, 230)).Item1, 
+                
+               (Color.FromArgb(242, 60, 242), Color.FromArgb(211, 63, 254), Color.FromArgb(173, 70, 255), Color.FromArgb(123, 79, 255), Color.FromArgb(0, 88, 255), Color.FromArgb(0, 96, 255)).Item2, LinearGradientMode.BackwardDiagonal);
+            g.FillRectangle(lgb, area);
+
+           
+            
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
